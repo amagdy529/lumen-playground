47f4a533dd569d6f88c7cc1f544c57ad6f31946a
amagdy529 <amagdy529@gmail.com>
2017-11-09 12:42:40 +0200

    create post validation V1

--- playground/app/Http/Controllers/PostController.php
+++ playground/app/Http/Controllers/PostController.php
@@ -36,7 +36,61 @@ class PostController extends Controller
         // return view('post.index', ['posts' => $posts]);
     }
 
-    
+
+    public function create(Request $request){
+        
+        $errors_arr = array();
+       
+        if (!$request->has('title')) {
+            return response()->json(['status' => "title isn't set!!"]);
+        }
+        if (!$request->has('body')) {
+            return response()->json(['status' => "body isn't set!!"]);
+        }
+
+        if(empty($request->title)){
+            $errors_arr [] = "empty title field" ;
+        }
+        if(empty($request->body)){
+            $errors_arr [] = "empty body field" ;
+        }
+
+      
+        if (sizeof($errors_arr) > 0) {
+            $false_arr = array(
+                "status" => false,
+                "status_msg" => $errors_arr,
+            );
+            return $false_arr;
+        }
+
+        
+        $post = new $this->post;
+        if (!empty($request->title)) {
+            $post->title = $request->title;
+            $post->save();    
+        }
+        if (!empty($request->body)) {
+            $post->body = $request->body;
+            $post->save();    
+        }
+        $post_obj = array(
+            'ID' => $post->id , 
+            'title' => $post->title , 
+            'body' => $post->body , 
+            'body' => $post->body , 
+            'updated_at' => $post->updated_at , 
+            'created_at' => $post->created_at , 
+        ); 
+        if ($post->save()) {
+            return response()->json(['status' => 'saved new post successfully' , 'post'=>$post_obj]);
+        }else{
+            return response()->json(['status' => 'Failed to save new post !!']);
+        }
+
+    }
+
+
 
     
 
